2ead67425509da62db378d9933afb7235a26e206
Mohaimen Khan <mohaimen.khan@mail.utoronto.ca>
2017-08-02 20:51:21 -0400

    Deleting makefile

--- build/Debug/Cygwin-Windows/login_header.o.d
+++
@@ -1,4 +0,0 @@
-build/Debug/Cygwin-Windows/login_header.o: login_header.cpp \
- login_header.h
-
-login_header.h:
--- build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp login_header.h
-
-login_header.h:
